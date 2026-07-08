2ae65e9b1f5aa5786a1081d423cfec91db20db8b
Roman Eliseev <eleseev2479@gmail.com>
2026-07-08 18:52:44 +0300

    Added implementation for stat command

--- FileCabinetService.cs
+++ FileCabinetService.cs
@@ -16,13 +16,12 @@ namespace filecabinet
         public FileCabinetRecord[] GetRecords()
         {
             // TODO: добавьте реализацию метода
-            return new FileCabinetRecord[] { };
+            return Array.Empty<FileCabinetRecord>();
         }
 
         public int GetStat()
         {
-            // TODO: добавьте реализацию метода
-            return 0;
+            return this.list.Count;
         }
     }
 }
