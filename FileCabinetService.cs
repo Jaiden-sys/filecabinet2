30b520e5ad71569535dad51d375f0eb2a3d0f6ea
Roman Eliseev <eleseev2479@gmail.com>
2026-07-22 23:17:48 +0300

    Improve find firstname performance with dictionary.

--- FileCabinetService.cs
+++ FileCabinetService.cs
@@ -10,16 +10,16 @@ namespace filecabinet
         private readonly Dictionary<string, List<FileCabinetRecord>> firstNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short archiveId, decimal weight, char type)
         {
-            if(firstName == null) { throw new ArgumentNullException(nameof(firstName), "firstname cannot be null"); }
+            if (firstName == null) { throw new ArgumentNullException(nameof(firstName), "firstname cannot be null"); }
             if (lastName == null) { throw new ArgumentNullException(nameof(lastName), "lastname cannot be null"); }
-            if (firstName.Length < 2 || firstName.Length > 60 || string.IsNullOrWhiteSpace(firstName))  throw new ArgumentException("Invalid first name", nameof(firstName));
+            if (firstName.Length < 2 || firstName.Length > 60 || string.IsNullOrWhiteSpace(firstName)) throw new ArgumentException("Invalid first name", nameof(firstName));
 
             if (lastName.Length < 2 || lastName.Length > 60 || string.IsNullOrWhiteSpace(lastName)) throw new ArgumentException("Invalid last name", nameof(lastName));
 
             if (dateOfBirth == default(DateTime))
                 throw new ArgumentException("Date of birth is not specified", nameof(dateOfBirth));
 
-            if (dateOfBirth < new DateTime(1950,1,1) || dateOfBirth > DateTime.Today)
+            if (dateOfBirth < new DateTime(1950, 1, 1) || dateOfBirth > DateTime.Today)
                 throw new ArgumentException("Invalid date of birth", nameof(dateOfBirth));
 
             if (archiveId <= 0)
@@ -28,26 +28,34 @@ namespace filecabinet
             if (weight <= 0 || weight > 200)
                 throw new ArgumentOutOfRangeException(nameof(weight), "Weight must be between 0 and 200");
 
-            if (type == ' ') 
+            if (type == ' ')
                 throw new ArgumentException("Type cannot be empty/space", nameof(type));
             var record = new FileCabinetRecord
-                {
-                    Id = this.list.Count + 1,
-                    FirstName = firstName,
-                    LastName = lastName,
-                    DateOfBirth = dateOfBirth,
-                    ArchiveId = archiveId,
-                    Weight = weight,
-                    Type = type
-                };
-                list.Add(record);
-                this.firstNameDictionary.Add(firstName,list);
-                return record.Id;
+            {
+                Id = this.list.Count + 1,
+                FirstName = firstName,
+                LastName = lastName,
+                DateOfBirth = dateOfBirth,
+                ArchiveId = archiveId,
+                Weight = weight,
+                Type = type
+            };
+
+            list.Add(record);
+            //nameList is the List with records satisfying firstname
+            if (!firstNameDictionary.TryGetValue(firstName, out List<FileCabinetRecord> nameList))
+            {
+                nameList = new List<FileCabinetRecord>();
+                firstNameDictionary.Add(firstName, nameList);
+            }
+            nameList.Add(record);
+
+            return record.Id;
         }
 
         public FileCabinetRecord[] GetRecords()
         {
-            
+
             return this.list.ToArray();
         }
 
@@ -58,19 +66,48 @@ namespace filecabinet
 
         public void EditRecord(int id, string firstName, string lastName, DateTime dateOfBirth, short archiveId, decimal weight, char type)
         {
-            List<FileCabinetRecord> foundList = firstNameDictionary.GetValueOrDefault(firstName);
-            foreach (var record in foundList) //NullReferenceException
+            FileCabinetRecord? recordToUpdate = null;
+            string? oldFirstName = null;
+            foreach (var pair in firstNameDictionary)
             {
-                if (record == null) throw new ArgumentException("Cannot find record with current id ");
-                record.FirstName = firstName;
-                record.LastName = lastName;
-                record.DateOfBirth = dateOfBirth;
-                record.ArchiveId = archiveId;
-                record.Weight = weight;
-                record.Type = type;
-                Console.WriteLine($"#{record.Id} was updated");
+                var found = pair.Value.FirstOrDefault(r => r.Id == id);
+                if (found != null)
+                {
+                    recordToUpdate = found;
+                    oldFirstName = pair.Key;
+                    break;
+                }
             }
+            if (recordToUpdate == null) throw new ArgumentException("Record not found");
+
+            if(oldFirstName != firstName)
+            {
+                var oldList = firstNameDictionary[oldFirstName];
+                oldList.Remove(recordToUpdate);
+
+                if(oldList.Count == 0)
+                {
+                    firstNameDictionary.Remove(oldFirstName);
+                }
+
+                if(!firstNameDictionary.TryGetValue(firstName,out var newList))
+                {
+                    newList = new List<FileCabinetRecord>();
+                    firstNameDictionary.Add(firstName, newList);
+                }
+                newList.Add(recordToUpdate);
+            }
+            recordToUpdate.FirstName = firstName;
+            recordToUpdate.LastName = lastName;
+            recordToUpdate.DateOfBirth = dateOfBirth;
+            recordToUpdate.ArchiveId = archiveId;
+            recordToUpdate.Weight = weight;
+            recordToUpdate.Type = type;
+            Console.WriteLine($"#{recordToUpdate.Id} was updated");
+
         }
+            
+        
 
         public FileCabinetRecord[] FindByField(string fieldName, string value)
         {
@@ -81,11 +118,11 @@ namespace filecabinet
                 case "lastname":
                     return list.Where(x => x.LastName.ToLower() == value.ToLower()).ToArray();
                 case "dateofbirth":
-                    return list.Where(x => x.DateOfBirth.ToString(value) == value.ToLower()).ToArray();  
+                    return list.Where(x => x.DateOfBirth.ToString(value) == value.ToLower()).ToArray();
                 default:
                     throw new ArgumentException($"Field {fieldName} isn't supported");
             }
-            
+
         }
     }
 }
